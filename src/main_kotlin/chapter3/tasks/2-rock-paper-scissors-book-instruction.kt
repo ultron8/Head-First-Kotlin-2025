931514a39ee69da13bf47c768432a14813df93c3
ultron8 <yashar@horizonta8.com>
2025-05-07 18:20:48 +0200

    add-chapter3-task2

--- src/main_kotlin/chapter3/tasks/2-rock-paper-scissors-book-instruction.kt
+++ src/main_kotlin/chapter3/tasks/2-rock-paper-scissors-book-instruction.kt
@@ -2,39 +2,40 @@ package main_kotlin.chapter3.tasks
 
 import kotlin.random.Random
 
-fun main(){
+fun main() {
 
-    // create the array
-//    val trpArray = arrayOf("Rock", "Scissors", "Paper")
-//    getGameChoice()
+    // create an array of r, s, p to randomly pick one of them
+    val options = arrayOf("Rock", "Scissors", "Paper")
 
-}
+    // create an object of getGameChoice
+    val gameChoice = getGameChoice(options)
+//    println("this is the game choice number: ${gameChoice}")
 
-fun getGameChoice(){
+} // fun main()
+
+
+fun getGameChoice(options_param: Array<String>) {
+
     // which will choose "Rock", "Scissors", "Paper" at random
-    val trpArray = arrayOf("Rock", "Scissors", "Paper")
-    val arraySize = trpArray.size
-    var randomNum = Random.nextInt(trpArray.size)
-
-    // picks randomly one of the elements in the array
-    var gameChoice = trpArray[randomNum]
+    var symbol = options_param[(Math.random() * options_param.size).toInt()]
+    println("This is the game choice symbol: $symbol")
 
 } // getGameChoice()
 
-fun getUserChoice(){
+
+fun getUserChoice() {
+
     // enter your choice
     // check for validChoice
         // if not keep ask them unitl they do
     println("Pls enter one of the following: Rock, Paper, Scissors.\n")
     var userChoice = readLine()
 
+
     if (userChoice != "Rock" || userChoice != "Paper" || userChoice != "Scissors") {
         println("Pls enter one of the following: Rock, Paper, Scissors.")
         println("Errr... dunno.")
         println("You must enter a valid choice.")
     }
-
-
-
 
 } // getUserChoice()
