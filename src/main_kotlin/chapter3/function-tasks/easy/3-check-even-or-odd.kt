b1a652beefd2e75df2dcb574dd30a5b426c21db6
ultron8 <yashar@horizonta8.com>
2025-05-10 11:16:55 +0200

    add-chapter3-function-tasks-easy-3

---
+++ src/main_kotlin/chapter3/function-tasks/easy/3-check-even-or-odd.kt
@@ -0,0 +1,35 @@
+// Task 3: Check Even or Odd
+// Write a function isEven that takes an integer
+// and returns true if it’s even, otherwise false.
+
+
+fun main() {
+
+    println("What is your number?\n")
+    var someNum = Integer.valueOf(readLine())
+
+    var chkNumber = isEven(someNum)
+
+    if (chkNumber == true) {
+        println("the number $someNum is even - $chkNumber")
+    } else {
+        println("the number $someNum is odd - $chkNumber")
+    }
+
+} // fun main
+
+fun isEven(x_param: Int?): Boolean {
+
+    var isEven = false
+
+    if (x_param != null) {
+        if ((x_param % 2) == 0) {
+            isEven = true
+            return isEven
+        }// if
+    } // if
+
+    isEven = false
+    return isEven
+
+}
