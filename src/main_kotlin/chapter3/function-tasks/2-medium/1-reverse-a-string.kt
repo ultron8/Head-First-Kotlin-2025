d60c1fc3ba68fe4fecec7ab66c08b17c014eaba9
ultron8 <yashar@horizonta8.com>
2025-05-10 11:54:52 +0200

    add-chapter3-function-tasks-changed-folder-structure

---
+++ src/main_kotlin/chapter3/function-tasks/2-medium/1-reverse-a-string.kt
@@ -0,0 +1,20 @@
+package main_kotlin.chapter3.`function-tasks`.`2-medium`
+
+// Task 1: Reverse a String
+// Description:
+// Write a function reverseString that takes a String and returns the reversed version.
+
+fun main() {
+
+    var someStr = readLine()
+
+    val revStr = reverseString(someStr)
+
+}
+
+fun reverseString(str_param: String): String {
+
+    var reversedStr = str_param.reversed()
+
+
+}
