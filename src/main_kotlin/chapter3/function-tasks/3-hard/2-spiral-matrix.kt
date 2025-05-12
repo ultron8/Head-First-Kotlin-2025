42a57fd5bcdf0d061ba02ba756945b8ed92f8cf3
ultron8 <yashar@horizonta8.com>
2025-05-12 18:28:20 +0200

    add-chapter3-function-tasks-hard-2

---
+++ src/main_kotlin/chapter3/function-tasks/3-hard/2-spiral-matrix.kt
@@ -0,0 +1,34 @@
+/*
+* 🔴 Task 2: Spiral Matrix
+* Description:
+* Write a function spiralOrder that returns a list of integers
+* representing the elements of a 2D matrix in spiral order.
+*
+*
+* I don't understand the task
+* */
+
+fun main() {
+
+    var listInList = listOf(
+        listOf(1, 2, 3),
+        listOf(4, 5, 6),
+        listOf(7, 8, 9)
+    )
+
+    var spiral = spiralOrder(listInList) // returns [1, 2, 3, 6, 9, 8, 7, 4, 5]
+
+    println(listInList)
+
+
+
+} // fun main
+
+fun spiralOrder(matrix : List<List<Int>>): List<Int> {
+
+    var someInt: MutableList<Int> = mutableListOf()
+
+
+    return someInt
+
+} // fun spiralOrder
