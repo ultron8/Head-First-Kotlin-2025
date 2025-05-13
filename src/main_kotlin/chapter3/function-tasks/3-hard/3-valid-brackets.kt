90b52612c348de6ff0b18466e609db32e1ac4c05
ultron8 <yashar@horizonta8.com>
2025-05-13 09:15:38 +0200

    add-chapter3-function-tasks-hard-3

---
+++ src/main_kotlin/chapter3/function-tasks/3-hard/3-valid-brackets.kt
@@ -0,0 +1,21 @@
+/*
+*
+* 🔴 Task 3: Valid Brackets
+Description:
+Write a function isValid that checks whether a string containing only ()[]{} is valid. A string is valid if brackets are closed in the correct order.
+
+kotlin
+Copy
+Edit
+fun isValid(s: String): Boolean {
+    // Your implementation
+}
+Example:
+
+kotlin
+Copy
+Edit
+isValid("({[]})") // returns true
+isValid("({[})")  // returns false
+*
+* */
