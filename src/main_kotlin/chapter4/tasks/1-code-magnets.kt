ff945d09c5c07d7631889cf6992ab7e7fcd72204
ultron8 <yashar@horizonta8.com>
2025-05-15 20:23:10 +0200

    add-chapter4-tasks-1-add-init-and-lateinit

--- src/main_kotlin/chapter4/tasks/1-code-magnets.kt
+++ src/main_kotlin/chapter4/tasks/1-code-magnets.kt
@@ -1,5 +1,16 @@
 class DrumKit(var hasTopHat: Boolean, var hasSnares: Boolean) {
 
+    // initializing before using variables
+    var volume : Int
+
+    init {
+        volume = 10
+    }
+
+    // late initialization - tells the compiler that we handle it later
+    lateinit var playlist : MutableList<String>
+
+
     // What a DrumKit can do ?
     fun playTopHat() {
         if (hasTopHat) {
@@ -30,6 +41,8 @@ fun main() {
 
     d.playTopHat()
     d.playSnare()
+
+    println("How high is the volume: ${d.volume}")
 
 } // fun main
 
