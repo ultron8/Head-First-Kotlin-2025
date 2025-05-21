cc05b759d52522df21bb1c285cf010b05e1a0a78
ultron8 <yashar@horizonta8.com>
2025-05-21 20:51:38 +0200

    add-chapter4-tasks-0-add-getter-and-setter

--- src/main_kotlin/chapter4/tasks/0-dog-class-object.kt
+++ src/main_kotlin/chapter4/tasks/0-dog-class-object.kt
@@ -1,7 +1,21 @@
 // dog class and object
 
 
-class Dog(val name: String, var weight: Int, val breed: String) {
+class Dog(val name: String, weight_param: Int, val breed: String) {
+
+
+    var weight = weight_param
+        set(value) {
+            if (value > 0 ) field = value else println("KGs cant be negative")
+        }
+
+    var weightInKgs: Double = 0.0
+        // this line defines the getter
+        get() = weight / 2.2
+        // weight can only updated to a value > 0
+            // move the weight property definition from the constructor -> "var weight: Int"   to the class body
+
+
 
     // behaviour
     fun bark() {
@@ -15,10 +29,10 @@ fun main(){
     println(myDog) // output is
 
     // access name of myDog
-    println(myDog.name)
+    println("myDog name: ${myDog.name}")
 
     // access weight
-    println(myDog.weight)
+    println("myDog weight: ${myDog.weight}")
 
     // access breed
     println(myDog.breed)
@@ -37,6 +51,37 @@ fun main(){
     dogs[1].weight = 15
     println("Ripper is barking: ${dogs[1].bark()}")
     println(dogs[1].weight)
+
+
+    // Dog with negative weight would be a bad thing
+//    myDog.weight_param = -1
+//    var negativeWeightDog = myDog.weight_param
+//    println("The Dog has a negative weight of (is that possible?): $negativeWeightDog")
+
+    // a custom getter to return the Dog's weight in kg
+    // asking for the value of the property weightInKgs
+
+
+    // weight obj
+    myDog.weight = 75
+    println("Weight in KGs is ${myDog.weightInKgs}")
+
+    myDog.weight = -2
+    // minus 2 will be not considered bc of the setter function in the class Dog
+    println("Weight is ${myDog.weight}")
+
+
+    // weightInKgs
+//    myDog.weightInKgs = 20.0
+//    var dogLBtoKG = myDog.weightInKgs
+//    println("the weight of the dog in kg is: $dogLBtoKG")
+//
+//
+//    // a custom setter to validate a proposed value for the Dog's weight before we assign it
+//    myDog.weightInKgs = -1.0
+//    var test = myDog.weightInKgs
+//    println("weight in KGs (is it negative or positive?): $test")
+
 
 
 
