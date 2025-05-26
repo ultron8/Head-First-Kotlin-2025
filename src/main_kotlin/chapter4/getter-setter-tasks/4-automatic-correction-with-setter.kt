55f066a940bb4096773f24317558853e5d8f4a5a
ultron8 <yashar@horizonta8.com>
2025-05-26 21:25:03 +0200

    add-chapter4-getter-setter-tasks-4

--- src/main_kotlin/chapter4/getter-setter-tasks/4-automatic-correction-with-setter.kt
+++ src/main_kotlin/chapter4/getter-setter-tasks/4-automatic-correction-with-setter.kt
@@ -6,3 +6,27 @@
 *
 * */
 
+
+fun main() {
+
+    val testAbsoluteZero = Temperature(-274.16)
+    println("What is the temperature? \n${testAbsoluteZero.celsius}")
+
+}
+
+class Temperature(_celsius : Double) {
+    var celsius: Double = 0.0
+        set(value) {
+            if (value < -273.15) {
+                println("Celsius cannot be below absolute zero")
+            } else {
+                field = value
+            } // if
+
+        } // setter
+
+    init {
+        celsius = _celsius // uses the setter
+    }
+
+} // class Tempera
